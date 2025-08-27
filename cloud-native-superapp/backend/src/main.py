8fb3adb9f26f38ffab6c3b97695a6ce592aaf1fb
Dardan Rashiti <rashitidardan3@gmail.com>
2025-08-27 16:05:39 +0000

    when we run this does it collects data ?

--- cloud-native-superapp/backend/src/main.py
+++ cloud-native-superapp/backend/src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+
+# Add Prometheus metrics
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
